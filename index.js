58b255d2293d289ff36a1b7b66f9176c397372fb
ivan <ivanteotonio245@gmil.com>
2022-02-03 01:47:53 -0300

    aprendendo a exibir variveies no ejs

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express();
 //configurando o ejs
 app.set('view engine','ejs');
 
-app.get("/",(req,res) => {
-    res.render('principal/perfil')
+app.get("/:nome/:lang",(req,res) => {
+    let nome = req.params.nome
+    let lang = req.params.lang
+    res.render('index',{
+        nome:nome,
+        lang:lang,
+        empresa:'Guia do proramador',
+        inscritos:8000
+    })
 });
 
 app.listen(8080,() =>{
